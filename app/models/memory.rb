873c658f305da8154c8f59016ac7da65ac3783f5
Kyle Kwon <kyleskwon@gmail.com>
2016-02-02 21:33:13 -0800

    refactor views with slideshows

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,11 +2,11 @@ class Memory < ActiveRecord::Base
   belongs_to :user
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
-  has_attached_file :avatar, styles: { medium: "500 x 1000>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "480x800>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-  validates_length_of :title, :minimum => 3
-  validates_length_of :address, :minimum => 3
+  validates :title, :address, presence: true
+
   default_scope { order('date ASC') }
 
   after_create :upgrade_account, if: :account_upgradable?
