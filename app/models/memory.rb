c9b0d605056f0fbc920e466beb43af5a607ec326
Kyle Kwon <kyleskwon@gmail.com>
2016-02-02 20:34:58 -0800

    34carousel

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,7 +2,7 @@ class Memory < ActiveRecord::Base
   belongs_to :user
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
-  has_attached_file :avatar, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "500 x 1000>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   validates_length_of :title, :minimum => 3
