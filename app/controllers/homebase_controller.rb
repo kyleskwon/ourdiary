6e2ac94c6dd1a1f4706714550541775571ce105c
Kyle Kwon <kyleskwon@gmail.com>
2016-02-06 13:14:44 -0800

    add plan markers to homebase map

--- app/controllers/homebase_controller.rb
+++ app/controllers/homebase_controller.rb
@@ -3,7 +3,7 @@ class HomebaseController < ApplicationController
     #  @show_map = false
     @memory_markers =  current_user.all_memories.map {|memory| {lat: memory.latitude, long: memory.longitude, title: memory.title}}.flatten
     @plan_markers =  current_user.all_plans.map {|plan| {lat: plan.latitude, long: plan.longitude, title: plan.title}}.flatten
-
+    @item_markers =  current_user.all_items.map {|item| {lat: item.latitude, long: item.longitude, title: item.title}}.flatten
   end
 
 private
