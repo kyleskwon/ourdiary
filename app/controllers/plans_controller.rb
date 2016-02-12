e2442d3bc75ce12388e69339072c4ad87bd7ece9
Kyle Kwon <kyleskwon@gmail.com>
2016-02-12 11:34:16 -0800

    labels for plans and items

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,5 @@
 class PlansController < ApplicationController
-  before_action :set_plan, only: [:show, :edit, :update, :destroy]
+  before_action :set_plan, only: [:show, :edit, :search, :update, :destroy]
 
   # GET /plans
   def index
