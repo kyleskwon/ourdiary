ef202678ab349416d274c8d0addc9e4bbf5e8ebe
Kyle Kwon <kyleskwon@gmail.com>
2016-02-13 10:15:40 -0800

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208184845) do
+ActiveRecord::Schema.define(version: 20160213180735) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20160208184845) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "partner_email"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
