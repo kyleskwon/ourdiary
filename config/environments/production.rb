962d8c3e46c9c23926787070045041609ae08725
Kyle Kwon <kyleskwon@gmail.com>
2016-02-13 10:24:42 -0800

    edit production mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://floating-plains-80135.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -78,5 +79,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
   Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
-  config.action_mailer.default_url_options = { host: 'floating-plains-80135.herokuapp.com' }
+
 end
