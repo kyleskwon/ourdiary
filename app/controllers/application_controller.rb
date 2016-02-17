cee063f6a68c5bc9adcf1074cede477663e8d666
Kyle Kwon <kyleskwon@gmail.com>
2016-02-16 23:03:55 -0800

    proper indent up to helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,10 +26,7 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
-
     flash[:error] = "You need to be a premium user to access that feature"
     redirect_to(request.referrer || root_path)
   end
-
-
 end
