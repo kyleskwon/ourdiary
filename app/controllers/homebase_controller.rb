ce720e6c60198fee73d299287f366fe2633eebcc
Kyle Kwon <kyleskwon@gmail.com>
2016-07-08 19:43:57 -0400

    add plans to homebase calendar

--- app/controllers/homebase_controller.rb
+++ app/controllers/homebase_controller.rb
@@ -1,6 +1,7 @@
 class HomebaseController < ApplicationController
   def index
     @memories = current_user.all_memories
+    @plans = current_user.all_plans
     #  @show_map = false
     @memory_markers =  current_user.all_memories.map {|memory| {lat: memory.latitude, long: memory.longitude, title: memory.title}}.flatten
     @plan_markers =  current_user.all_plans.map {|plan| {lat: plan.latitude, long: plan.longitude, title: plan.title}}.flatten
